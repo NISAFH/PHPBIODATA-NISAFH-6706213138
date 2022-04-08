785f9f609a421e764245c76c89bc695d0a728c6f
NISAFH <91384557+NISAFH@users.noreply.github.com>
2022-04-08 23:09:29 +0700

    Add files via upload

---
+++ Biodata.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Biodata Nisa Fauziyyah Handari PHP </title>
+    </head><br/>
+    <body style = "background-color:#FFEEAD;">
+    <h1 style = "background-color:#F47340; color:antiquewhite"> <p align="center"> Homepage Pribadi </h1></p>
+    <pre><b><h2>
+        <?php
+       
+        $Nama = "Nisa Fauziyyah Handari";
+        $NIM = "6706213138";
+        $Kelas = "RPLA 45-05";
+        $Alamat = "Kp. Tonjong RT/RW 04/07 Desa Panundaan Kec. Ciwidey Kab. Bandung";
+        $RiwayatPendidikan = " ";
+        $SMA = "SMAN 1 Ciwidey";
+        $SMP = "SMPN 2 Ciwidey";
+        $TTL = "Batam, 22 Februari 2003 ";
+        $Email = "nisafauziyyah@student.telkomuniversity.ac.id";
+        $Hobby = "Tidak ada";
+        $DeskripsiPribadi = "Saya adalah anak baik dan pintar menabung. 
+        
+                                Saya mempunyai cita-cita menjadi orang kaya, agar bisa memberi tanpa mikir.";
+
+        echo"1. Nama               : $Nama<br><br>";
+        echo"        2. NIM                : $NIM<br><br>";
+        echo"        3. Kelas              : $Kelas<br><br>";
+        echo"        4. Alamat             : $Alamat<br><br>";
+        echo"        5. RiwayatPendidikan  : $RiwayatPendidikan<br><br>";
+        echo"              a. SMA          : $SMA<br><br>";
+        echo"              b. SMP          : $SMP<br><br>";
+        echo"        6. TTL                : $TTL<br><br>";
+        echo"        7. Email              : $Email<br><br>";
+        echo"        8. Hobby              : $Hobby<br><br><br>";
+        echo"        9. DeskripsiPribadi   : $DeskripsiPribadi<br><br><br>";
+        
+        ?>
+        </pre></b></h2>
+    </body>
+</html>
